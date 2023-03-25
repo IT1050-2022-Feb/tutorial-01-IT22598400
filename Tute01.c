1bc6fd1c87ba16c64390009d6b27ece4d9f51a16
IT22598400 <128892429+IT22598400@users.noreply.github.com>
2023-03-25 14:00:52 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,16 @@
 #include <stdio.h>
 
 int main() {
+   int i, marks = 0, totalMarks = 0;
+   for(i=1; i <= 2; i++){
+   
+      printf("Enter the subject %d", i);
+      scanf("%d", &marks);
+      totalMarks = marks + totalMarks;
+   
+   }
+   
+   printf("Average marks is %d", (totalMarks / 2);
   
   return 0;
 }
